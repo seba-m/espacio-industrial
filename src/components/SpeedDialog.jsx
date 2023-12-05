8b8d328c220684a4b7aa35a36bbc0c580a6e79ad
Arlezz <antonyrodriguezc.2001@gmail.com>
2023-12-05 11:39:23 -0300

    Refactor SpeedDialog component

--- src/components/SpeedDialog.jsx
+++ src/components/SpeedDialog.jsx
@@ -1,18 +1,18 @@
 import {
-    IconButton,
-    SpeedDial,
-    SpeedDialHandler,
-    SpeedDialContent,
-    SpeedDialAction,
-    Typography
+  IconButton,
+  SpeedDial,
+  SpeedDialHandler,
+  SpeedDialContent,
+  SpeedDialAction,
+  Typography,
 } from "@material-tailwind/react";
 import {
-    PlusIcon,
-    HomeIcon,
-    CogIcon,
-    Square3Stack3DIcon,
-    QuestionMarkCircleIcon,
-    ChatBubbleBottomCenterIcon
+  PlusIcon,
+  HomeIcon,
+  CogIcon,
+  Square3Stack3DIcon,
+  QuestionMarkCircleIcon,
+  ChatBubbleBottomCenterIcon,
 } from "@heroicons/react/24/outline";
 
 import { useRouter } from "next/router";
@@ -21,52 +21,64 @@ import { HelpModal } from "@/components/HelpModal";
 import { FeedbackModal } from "@/components/FeedbackModal";
 
 export function SpeedDialog() {
-    const [openHelp, setOpenHelp] = useState(false);
-    const [openFeedback, setOpenFeedback] = useState(false);
-    
-    const labelProps = {
-        variant: "small",
-        color: "blue-gray",
-        className:
-            "absolute top-2/4 -left-2/4 -translate-y-2/4 -translate-x-3/4 font-normal",
-    };
+  const [openHelp, setOpenHelp] = useState(false);
+  const [openFeedback, setOpenFeedback] = useState(false);
 
-    const router = useRouter();
+  const labelProps = {
+    variant: "small",
+    color: "blue-gray",
+    className:
+      "absolute top-2/4 -left-2/4 -translate-y-2/4 -translate-x-3/4 font-normal",
+  };
 
-    const handleHome = () => {
-        router.push("/");    
-    }
+  const router = useRouter();
 
-    return (
-        <>
-            <div className="fixed bottom-6 right-6 z-50">
-                <div className="absolute bottom-0 right-0">
-                    <SpeedDial placement="top">
-                        <SpeedDialHandler>
-                            <IconButton size="lg" className="rounded-full">
-                                <PlusIcon className="h-5 w-5 transition-transform group-hover:rotate-45" />
-                            </IconButton>
-                        </SpeedDialHandler>
-                        <SpeedDialContent>
-                            <SpeedDialAction onClick={handleHome}>
-                                <HomeIcon className="h-5 w-5" />
-                                <Typography {...labelProps}>Ir al inicio</Typography>
-                            </SpeedDialAction>
-                            <SpeedDialAction onClick={() => setOpenFeedback(true)}>
-                                <ChatBubbleBottomCenterIcon className="h-5 w-5" />
-                                <Typography {...labelProps}>Enviar comentarios</Typography>
-                            </SpeedDialAction>
-                            <SpeedDialAction onClick={() => setOpenHelp(true)} className="ml-2">
-                                <QuestionMarkCircleIcon className="h-5 w-5" />
-                                <Typography {...labelProps}>Ayuda</Typography>
-                            </SpeedDialAction>
-                        </SpeedDialContent>
-                    </SpeedDial>
-                </div>
-            </div>
-            <HelpModal open={openHelp} setOpen={setOpenHelp} />
-            <FeedbackModal open={openFeedback} setOpen={setOpenFeedback} />
-        </>
+  const handleHome = () => {
+    router.push("/");
+  };
 
-    );
+  return (
+    <>
+      <div className="fixed bottom-6 right-6 z-50">
+        <div className="absolute bottom-0 right-0">
+          <SpeedDial placement="top">
+            <SpeedDialHandler>
+              <IconButton size="lg" className="rounded-full bg-[#ffa500] shadow-xl">
+                <PlusIcon className="h-5 w-5 transition-transform group-hover:rotate-45" />
+              </IconButton>
+            </SpeedDialHandler>
+            <SpeedDialContent>
+              <SpeedDialAction onClick={handleHome}>
+                <HomeIcon className="h-5 w-5" />
+                <Typography {...labelProps}>
+                  <label className="bg-white p-2 rounded-full shadow-xl">
+                    Ir al inicio
+                  </label>
+                </Typography>
+              </SpeedDialAction>
+              <SpeedDialAction onClick={() => setOpenFeedback(true)}>
+                <ChatBubbleBottomCenterIcon className="h-5 w-5" />
+                <Typography {...labelProps}>
+                  <label className="bg-white p-2 rounded-full shadow-xl">
+                    Feedback
+                  </label>
+                </Typography>
+              </SpeedDialAction>
+              <SpeedDialAction
+                onClick={() => setOpenHelp(true)}
+                className="ml-2"
+              >
+                <QuestionMarkCircleIcon className="h-5 w-5" />
+                <Typography {...labelProps}>
+                  <label className="bg-white p-2 rounded-full shadow-xl">Ayuda</label>
+                </Typography>
+              </SpeedDialAction>
+            </SpeedDialContent>
+          </SpeedDial>
+        </div>
+      </div>
+      <HelpModal open={openHelp} setOpen={setOpenHelp} />
+      <FeedbackModal open={openFeedback} setOpen={setOpenFeedback} />
+    </>
+  );
 }
